6d9d0d5ad0a8e9b8a4533d60c12e2a71a8d53970
Zura Sekhniashvili <zurasekhniashvili@gmail.com>
2017-04-11 17:35:59 +0400

    First commit using yeoman gulp angular generator

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('angularLobiadmin')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
