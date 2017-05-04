3ff1ee1135d0fa9388028cee964f5a017e4099b7
GeorgeB <giorgi.batiashvili@gmail.com>
2017-05-04 18:37:23 +0400

    Calendar Module - Generated empty module

---
+++ src/app/main/apps/calendar/calendar.controller.js
@@ -0,0 +1,26 @@
+(function () {
+  'use strict';
+
+  angular
+      .module('app.calendar')
+      .controller('CalendarController', CalendarControllerFn);
+
+  /** @ngInject */
+  function CalendarControllerFn(){
+    // var vm = this;
+
+    // Data
+
+
+    // Methods
+
+
+    init();
+
+    ///////////
+
+    function init(){
+
+    }
+  }
+})();
